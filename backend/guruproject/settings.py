e9e040ed01331fd865c9a47433a9e7e8e6ddda06
Oleg <oruchkin@bk.ru>
2022-11-02 15:45:37 +0300

    csrf настройки для админки

--- backend/guruproject/settings.py
+++ backend/guruproject/settings.py
@@ -15,6 +15,7 @@ include(
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'https://127.0.0.1']
 SECRET_KEY = os.environ.get('SECRET_KEY', default=None),
 
 BASE_DIR = Path(__file__).resolve().parent.parent
